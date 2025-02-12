5cb86703553410cebd598acddd71f9f66cb375ee
Fernando94654 <A00841856@tec.mx>
2025-02-12 13:36:36 -0600

    12/2/25

--- Tensorflow/DeteccionHSU/cargaModelo.py
+++ Tensorflow/DeteccionHSU/cargaModelo.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_hub as hub
 import numpy 
 import cv2
 import matplotlib.pyplot as plt
@@ -21,28 +22,29 @@ data_train=data_train.cache()
 data_test=data_test.cache()
 """
 #carga de modelo e imagen de prueba
-model=tf.keras.models.load_model("C:\\Users\\ferna\\Documents\\vision_artificial\\Tensorflow\\DeteccionHSU\\redConvolucionalHSU.h5")
-image=cv2.imread("C:\\Users\\ferna\Documents\\vision_artificial\\Tensorflow\\DeteccionHSU\\fotos\\white2.jpg",cv2.IMREAD_GRAYSCALE)
+model=tf.keras.models.load_model("C:\\Users\\ferna\\Documents\\vision_artificial\\mobilenetModel\\HSU_detection_mobilenetv3.h5",
+    custom_objects={'KerasLayer': hub.KerasLayer})
+image=cv2.imread("C:\\Users\\ferna\Documents\\vision_artificial\\Tensorflow\\DeteccionHSU\\fotos\\white2.jpg")
 if image is None:
     print("error al cargar la imagen")
-image=cv2.resize(image,(28,28))
+image=cv2.resize(image,(224,224,3))
 #condicional para distinguir entre imagen en blanco o imagen con letra
-if numpy.all(image>100) :
-    print("blanco")
-else:
-    print("letra")
-    letter_detected=["H","S","U"]
-    #normalizar imagen 
-    image=image/255.0
-    image = numpy.expand_dims(image, axis=-1)  # Agregar un canal (grayscale)
-    image = numpy.expand_dims(image, axis=0)  # Agregar dimensión de batch
-    #mostrar imagen normalizada
-    cv2.imshow("Imagen", image.squeeze())  # Eliminar dimensiones extra para mostrar la imagen
-    # Espera a presionar una tecla para cerrar la imagen
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    #procesar resultado e imprimirlo
-    resultado=model.predict(image)
-    resultado=numpy.argmax(resultado)
-    resultado=letter_detected[resultado]
-    print(resultado)
+# if numpy.all(image>100) :
+#     print("blanco")
+# else:
+# print("letra")
+letter_detected=["H","S","U"]
+#normalizar imagen 
+image=image/255.0
+image = numpy.expand_dims(image, axis=-1)  # Agregar un canal (grayscale)
+image = numpy.expand_dims(image, axis=0)  # Agregar dimensión de batch
+#mostrar imagen normalizada
+cv2.imshow("Imagen", image.squeeze())  # Eliminar dimensiones extra para mostrar la imagen
+# Espera a presionar una tecla para cerrar la imagen
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#procesar resultado e imprimirlo
+resultado=model.predict(image)
+resultado=numpy.argmax(resultado)
+resultado=letter_detected[resultado]
+print(resultado)
